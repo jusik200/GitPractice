d8721b93646a3d0d05d2294b14e639c673a9cbd8
jusik200 <jusik200@gmail.com>
2022-05-08 16:09:20 +0600

    added sauceLabs class to gitignore

--- src/main/java/com/imoving/UI/utils/SauceLabsDemo1.java
+++ src/main/java/com/imoving/UI/utils/SauceLabsDemo1.java
@@ -12,9 +12,7 @@ import java.net.URL;
 public class SauceLabsDemo1 {
 
     public static WebDriver loadSauceLabs() throws MalformedURLException {
-        String USERNAME = "oauth-jusik200-1f1b9";
-        String ACCESS_KEY = "a856666a-a965-4ece-9c9d-d51b0741c325";
-        String url = "https://oauth-jusik200-1f1b9:a856666a-a965-4ece-9c9d-d51b0741c325@ondemand.eu-central-1.saucelabs.com:443/wd/hub";
+
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("platformName", Platform.WIN10);
